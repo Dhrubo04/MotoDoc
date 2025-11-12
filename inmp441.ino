4f675effb878c880562b1d2686056ac5e26c8e89
Dhrubo <132836713+Dhrubo04@users.noreply.github.com>
2025-11-12 22:17:49 +0530

    Change I2S_SD pin from 22 to 33

--- inmp441.ino
+++ inmp441.ino
@@ -17,7 +17,7 @@
 #include "driver/i2s.h"
 
 #define I2S_WS   25   // LRCLK
-#define I2S_SD   22   // DOUT
+#define I2S_SD   33   // DOUT
 #define I2S_SCK  26   // BCLK
 #define I2S_PORT I2S_NUM_0
 
